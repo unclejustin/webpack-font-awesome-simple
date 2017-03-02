d9a511240ea353ad2cd243918d84a8c847679c3d
Justin Boyson <unclejustin@gmail.com>
2017-03-01 21:57:43 -0600

    Merge font rules.

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,22 @@ const path = require('path');
 var config = {
     entry: './app.js',
     output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: "css-loader"
-            })
-        }, {
-            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url-loader?limit=10000&mimetype=application/font-woff"
-        }, {
-            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url-loader?limit=10000&mimetype=application/font-woff"
-        }, {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-        }, {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "file-loader"
-        }, {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-        }]
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000',
+            }
+        ]
     },
     plugins: [
         new ExtractTextPlugin("styles.css"),
