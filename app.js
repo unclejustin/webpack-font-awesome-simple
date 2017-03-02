84773a0e7bf56a29d5602dcc2e32771617ce2687
Justin Boyson <unclejustin@gmail.com>
2017-03-01 21:41:52 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+require('font-awesome/css/font-awesome.css');
+
+document.body.innerHTML = '<i class="fa fa-fw fa-check"></i>';
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var config = {
+    entry: './app.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        }, {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file-loader"
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
+};
+
+module.exports = config;
